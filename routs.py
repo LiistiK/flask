28022117a8000dfa6b304384f15f0567a56affbe
Гамид <95250214+LiistiK@users.noreply.github.com>
2023-01-03 21:30:37 +0300

    Update routs.py

--- routs.py
+++ routs.py
@@ -6,3 +6,5 @@ app = Flask("app")
 app.add_url_rule("/advertisements/<int:id_ad>/", view_func=AdView.as_view('advertisements_delete'),
                  methods=['DELETE', 'GET'])
 app.add_url_rule("/advertisements", view_func=AdView.as_view('advertisements_create'), methods=['POST'])
+
+app.run()
